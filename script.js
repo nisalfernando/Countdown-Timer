fee1fb27345080dc35894d9c4e405fe380a10813
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-09-30 12:35:56 +0200

    now variable return the current time

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var endDate = new Date("Dec 25, 2019 12:00:00").getTime();
 
-var timer = setInterval(function() {}, 1000);
+var timer = setInterval(function() {
+    let now = new Date().getTime();
+}, 1000);
