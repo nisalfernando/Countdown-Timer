1ca902228efe28053320d2ca9360ad929c2a2506
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-10-01 11:49:28 +0200

    Conerting the UTC time to readable format

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ var endDate = new Date("Dec 25, 2019 12:00:00").getTime();
 var timer = setInterval(function() {
     let now = new Date().getTime();
     let t = endDate - now;
+
+    // Convert UTC time
+    if (t >= 0) {
+    }
 }, 1000);
