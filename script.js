3172417cb28c7dc5353cb67a50aa8522d52d28ec
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-10-02 12:15:48 +0200

    Convert the seconds

--- script.js
+++ script.js
@@ -9,5 +9,6 @@ var timer = setInterval(function() {
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
     }
 }, 1000);
