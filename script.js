77f08d1578b5d4120bf5578c4ddace6edbf7a347
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-10-02 12:14:21 +0200

    Convert the minutes

--- script.js
+++ script.js
@@ -8,5 +8,6 @@ var timer = setInterval(function() {
     if (t >= 0) {
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     }
 }, 1000);
